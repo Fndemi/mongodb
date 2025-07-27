54c91241df9857e46fafcb8cd88a03e4329557db
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-27 09:51:22 +0300

    Commit:Successfully connected myAPI to MOngoDB and also added POST,GET,PATCH and DELETE methods

--- node/app.js
+++ node/app.js
@@ -3,7 +3,7 @@ const { ObjectId, MongoClient } = require('mongodb')
 const { connectToDb, getDb } = require('./db')
 //init app & mddleware
 const app = express()
-
+app.use(express.json()) //to parse the json into req.body
 
 
 //db connection
@@ -18,13 +18,20 @@ connectToDb((err) => {
   }
 })
 
-
 //routes
 app.get('/books', (req, res) => {
+  //current page
+  const page = req.query.p || 0
+  const booksPerPage = 3
+
+
+
   let books = []
   db.collection('books')//in the shell we had db.books
     .find() //returns a cursor toArray for Each-iterate each at a time so we cn process 
     .sort({ author: 1 })
+    .skip(page * booksPerPage)
+    .limit(booksPerPage)
     .forEach(book => books.push(book))
     .then(() => {
       res.status(200).json(books)
@@ -51,6 +58,52 @@ app.get('/books/:id', (req, res) => {
     res.status(500).json({ error: 'Not a valid doc id' })
   }
 
+})
+
+
+app.post('/books', (req, res) => {
+  const book = req.body
+  db.collection('books')
+    .insertOne(book)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({ err: 'Could not create a new document' })
+    })
+})
+
+
+app.delete('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Could not delete the document' })
+      })
+  }
+  else {
+    res.status(500).json({ error: 'Not a valid doc id' })
+  }
+})
+app.patch('/books/:id', (req, res) => {
+  const updates = req.body
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json({ error: 'Could not update the document' })
+      })
+  }
+  else {
+    res.status(500).json({ error: 'Not a valid doc id' })
+  }
 
 
 })

695300b0a07563e526a6c00e2ba86585458cf603
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-27 10:18:42 +0300

    Commit:coonected the app to Atlas successfully

--- node/db.js
+++ node/db.js
@@ -1,8 +1,9 @@
 const { MongoClient } = require('mongodb')
 let dbConnection
+let uri = 'mongodb+srv://mongoflo:testing123@cluster0.riveki9.mongodb.net/CompanyDB?retryWrites=true&w=majority&appName=Cluster0'
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/bookstore').then
+    MongoClient.connect(uri).then
       ((client) => {
         dbConnection = client.db()
         return cb()
